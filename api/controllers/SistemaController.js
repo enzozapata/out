ea8f4d698d75f27cdbd1804161cf584c0fa0e06b
Enzo Zapata <enzo0697@gmail.com>
2016-12-16 22:12:59 +0000

    SistemaController.js

---
+++ api/controllers/SistemaController.js
@@ -0,0 +1,11 @@
+/**
+ * SistemaController
+ *
+ * @description :: Server-side logic for managing Sistemas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
