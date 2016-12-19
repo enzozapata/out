e9126a9cb1e14c0d854408cd17f69e20f7311167
Enzo Zapata <enzo0697@gmail.com>
2016-12-19 02:14:56 +0000

    Redirector andando.

--- api/controllers/SistemaController.js
+++ api/controllers/SistemaController.js
@@ -7,15 +7,14 @@
 
 module.exports = {
 	index: function (req, res) {
-		var host=req.param('host');
+		var host=req.param('link');
 		var url=req.param('url');
 		var urlredir;
-		if(host==new Buffer('adfly').toString('base64')){
+		if(host=='YWRmbHk'){
 			urlredir='https://adf.ly/'+url;
-		} else if(host==new Buffer('linkbucks').toString('base64')){
+		} else if(host=='bGlua2J1Y2tz'){
 			urlredir='http://linkbucks.com/'+url;
 		}
-    return res.view('index.pug', urlredir);
+    return res.view('index.pug', {url:urlredir});
   }
 };
-
